9c72dab7e3c6e8316b3d741890f8c1625c9548d9
Ludwig Schwardt <ludwig@ska.ac.za>
2020-10-14 11:51:10 +0200

    Strip hashbang and normalise strings
    Nobody in their right mind will call setup.py directly...

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 ################################################################################
 # Copyright (c) 2020, National Research Foundation (SARAO)
 #
@@ -53,8 +51,8 @@ setup(name="katbeam",
       platforms=["OS Independent"],
       keywords="meerkat ska",
       zip_safe=False,
-      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
-      setup_requires=['katversion'],
+      python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
+      setup_requires=["katversion"],
       use_katversion=True,
       install_requires=[
           "numpy",
